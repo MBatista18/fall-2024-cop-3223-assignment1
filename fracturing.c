7a1428f994d9446ee82e2be792a4f5413a02b8c6
Michael Batista <149213942+MBatista18@users.noreply.github.com>
2024-09-04 02:14:57 +0000

    Fracturing Progress

--- fracturing.c
+++ fracturing.c
@@ -1,1 +1,55 @@
+//********************************************************
+// fracturing.c
+// Author: Michael Batista / ID: 5466029
+// Date: 09/03/2024
+// Class: COP 3223C, Professor Parra
+// Purpose: 
+// Input: 
+//
+// Output: 
+// //********************************************************
 
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char **argv){
+    calculateDistance();
+    return 0;
+}
+
+double calculateDistance(){
+    double x1, y1, x2, y2;
+
+    printf("Enter x1: \n");
+    scanf("%lf", &x1);
+    printf("Enter y1: \n");
+    scanf("%lf", &y1);
+
+    printf("Enter x2: \n");
+    scanf("%lf", &x2);
+    printf("Enter y2: \n");
+    scanf("%lf", &y2);
+
+    printf("Point #1 entered x1 = %lf; y1 = %lf\n", x1, y1);
+    printf("Point #2 entered x2 = %lf; y2 = %lf\n", x2, y2);
+
+    double distance = sqrt(pow(x2 - x1, 2)+pow(y2 - y1, 2));
+
+    return distance;
+}
+
+double calculatePerimeter(){
+
+}
+
+double calculateArea(){
+
+}
+
+double calculateWidth(){
+
+}
+
+double calculateHeight(){
+
+}
